4d393a6b0c08c4f41823d7d4dea9f5b1805fd20c
DorkWitAFork <75294127+DorkWitAFork@users.noreply.github.com>
2025-01-29 06:29:43 -0800

    Update ComplexPlane.cpp

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
-{
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight) {
 	m_pixel_size.x = pixelWidth;
 	m_pixel_size.y = pixelHeight;
 	m_aspectRatio = (float)pixelHeight / (float)pixelWidth;
@@ -19,22 +18,17 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 	m_vArray.resize(pixelWidth * pixelHeight);
 }
 
-void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
-{
+void ComplexPlane::draw(RenderTarget& target, RenderStates states) const {
 	target.draw(m_vArray);
 }
 
-void ComplexPlane::updateRender()
-{
+void ComplexPlane::updateRender() {
 	// Only do this if it's calculating
-	if (m_state == State::CALCULATING)
-	{
+	if (m_state == State::CALCULATING) {
 		// rows
-		for (int i = 0; i < m_pixel_size.y; i++)
-		{
+		for (int i = 0; i < m_pixel_size.y; i++) {
 			// columns
-			for (int j = 0; j < m_pixel_size.x; j++)
-			{
+			for (int j = 0; j < m_pixel_size.x; j++) {
 				// store the vertex
 				m_vArray[i * m_pixel_size.x + j].position = { (float)j,(float)i };
 				Vector2f coord = mapPixelToCoords({ j,i }); // its position on the plane
@@ -49,8 +43,7 @@ void ComplexPlane::updateRender()
 	}
 }
 
-void ComplexPlane::zoomIn()
-{
+void ComplexPlane::zoomIn() {
 	m_zoomCount++;
 	float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
 	float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
@@ -59,8 +52,7 @@ void ComplexPlane::zoomIn()
 	m_state = State::CALCULATING; // Revert to calculating so it can redraw at the new spot
 }
 
-void ComplexPlane::zoomOut()
-{
+void ComplexPlane::zoomOut() {
 	m_zoomCount--;
 	float x_size = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
 	float y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
@@ -68,8 +60,7 @@ void ComplexPlane::zoomOut()
 	m_state = State::CALCULATING; // Revert to calculating so it can redraw at the new spot
 }
 
-void ComplexPlane::setCenter(Vector2i mousePixel)
-{
+void ComplexPlane::setCenter(Vector2i mousePixel) {
 	/*
 	The crucial part : setting the center of the plane to the coordinates of the mouse,
 	wherever they click will be the new center 
@@ -78,14 +69,12 @@ void ComplexPlane::setCenter(Vector2i mousePixel)
 	m_state = State::CALCULATING;
 }
 
-void ComplexPlane::setMouseLocation(Vector2i mousePixel)
-{
+void ComplexPlane::setMouseLocation(Vector2i mousePixel) {
 	// Not much here, just mapping again
 	m_mouseLocation = mapPixelToCoords(mousePixel); 
 }
 
-void ComplexPlane::loadText(Text& text)
-{
+void ComplexPlane::loadText(Text& text) {
 	stringstream ss;
 	ss << "Mouse Location: (" << m_mouseLocation.x << ", " << m_mouseLocation.y << ")" << endl;
 	ss << "Center: (" << m_plane_center.x << ", " << m_plane_center.y << ")" << endl;
@@ -96,8 +85,7 @@ void ComplexPlane::loadText(Text& text)
 }
 
 
-size_t ComplexPlane::countIterations(Vector2f coord)
-{
+size_t ComplexPlane::countIterations(Vector2f coord) {
 	// Create our complex number from the coordinates
 	complex<float> c;
 	c.real(coord.x); // Kept this separate for dubugging/readability
@@ -107,31 +95,25 @@ size_t ComplexPlane::countIterations(Vector2f coord)
 	size_t count = 0; // To keep track of how many iterations we've done
 
 	// This goes until we either get past 2, or we read the maximumn iterations
-	while (abs(z) < 2 && count < MAX_ITER)
-	{
+	while (abs(z) < 2 && count < MAX_ITER) {
 		z = z * z + c;
 		count++;
 	}
 	return count;
 }
 
-void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
-{
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b) {
 	// set to black if count eaches max_iter, meaning it didn't escape.
-	if (count == MAX_ITER)
-	{
+	if (count == MAX_ITER) {
 		r = 0;
 		g = 0;
 		b = 0;
-	}
-	else
-	{
+	} else {
 		// establish the region and color.
 		int region = count / (MAX_ITER / 5);
 		int color = count % 5;
 
-		switch (region)
-		{
+		switch (region) {
 		case 0:
 			r = 255 - color * 255 / (MAX_ITER / 5);
 			g = 0;
@@ -161,8 +143,7 @@ void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 	}
 }
 
-Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
-{
+Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel) {
 	// Help us find their position relative to the size of the window
 	//float windowPosX = (mousePixel.x - 0) / static_cast<float>(m_pixel_size.x);
 	//float windowPosY = (mousePixel.y - m_pixel_size.y) / static_cast<float>(-m_pixel_size.y);
